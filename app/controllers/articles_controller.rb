970cc1dd2df9a2b3918b3ad75d17868c72c01ee4
Edgar Minault <e.minault@bluewin.ch>
2020-05-12 15:47:47 +0200

    New post fct created. Gem of bootstrap form installed. Styling the form.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,22 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new # needed to instantiate the form_for
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+
+    # no need for app/views/restaurants/create.html.erb
+    redirect_to article_path(@article)
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
 end
